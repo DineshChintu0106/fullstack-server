9908255f4d3b46f6a953f5d6be4023e105def1a7
DineshChintu0106 <dineshchintu0106@gmail.com>
2023-07-29 14:37:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 // const {PORT}  = require('dotenv')
 const cors = require('cors')
 const app = express();
-const PORT = 4000
+
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/',(request,response) => {
-    const data = [{name:'dinesh',age:23}]
-    response.json(data)
+
+const url = "mongodb+srv://restaurantbooking:root@cluster0.cbkcrne.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started at ${PORT} `)
+app.get('/api', async (req, res) => {
+    try {
+        const collection = client.db('Restaurants').collection('RestaurantsList');
+        const allData = await collection.find({}).toArray();
+
+        res.json(allData);
+    } catch (error) {
+        console.error('Error retrieving data:', error);
+        res.status(500).json({ error: 'Error retrieving data' });
+    }
+});
+
+app.listen(4000, async () => {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB successfully!');
+        console.log("server started")
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
 })
