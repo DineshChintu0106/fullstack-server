b5d12b3e0389c17df1936e4b436e9dfeddef8628
Shivakumar0199 <shivad2727@gmail.com>
2023-07-29 17:38:43 +0530

    Created API's

--- index.js
+++ index.js
@@ -25,7 +25,51 @@ app.get('/api', async (req, res) => {
         const collection = client.db('Restaurants').collection('RestaurantsList');
         const allData = await collection.find({}).toArray();
 
-        res.json(allData);
+        res.send(allData);
+    } catch (error) {
+        console.error('Error retrieving data:', error);
+        res.status(500).json({ error: 'Error retrieving data' });
+    }
+});
+
+app.post('/RestuarantList', async(req,res) =>{
+    try{
+        const collection = client.db('Restaurants').collection('Barbeque Nation');
+        const restList = await collection.insertOne({"Name":"Helapuri Restuarant",
+        "Food items":[{"item":"Veg Fried rice","Favourites":false}, {"item":"Chicken Fried rice","Favourites":false}, {"item":"Chilli Chicken","Favourites":false}, {"item":"Chicken Manchurian","Favourites":false}],
+        "Availability":true}
+        )
+        res.send(restList)
+        console.log(restList)
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.put('/updateList', async(req,res) =>{
+    try{
+        const collection = client.db('Restaurants').collection('RestaurantsList');
+        const restList = await collection.updateMany({
+            "Name":"Cascades"},
+            {$set :{"Food items":[{"item":"Veg Fried rice","Favourites":false}, {"item":"Chicken Fried rice","Favourites":false}, {"item":"Chilli Chicken","Favourites":false}, {"item":"Chicken Manchurian","Favourites":false}] }}
+        )
+        res.send(restList)
+        console.log(restList)
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.delete('/deleteList', async (req, res) => {
+    try {
+        const collection = client.db('Restaurants').collection('RestaurantsList');
+        const allData = await collection.deleteMany({
+            "name":"Alpha"
+        });
+
+        res.send(allData);
     } catch (error) {
         console.error('Error retrieving data:', error);
         res.status(500).json({ error: 'Error retrieving data' });
@@ -36,7 +80,7 @@ app.listen(4000, async () => {
     try {
         await client.connect();
         console.log('Connected to MongoDB successfully!');
-        console.log("server started")
+        console.log("http://localhost:4000")
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
