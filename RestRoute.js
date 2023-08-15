5b32d82c4a158884a934def046257c14e2070e81
Shivakumar0199 <shivad2727@gmail.com>
2023-08-15 11:01:08 +0530

    Added exports & imports

---
+++ RestRoute.js
@@ -0,0 +1,22 @@
+const RestController = require("./RestController");
+
+module.exports = (app) =>{
+
+    app.get("/api", RestController.getData);
+
+    app.get("/fetchDetails/:id", RestController.getId);
+
+    app.post("/login", RestController.login);
+
+    app.post("/register", RestController.register);
+
+    app.post("/RestuarantList", RestController.restuarantList);
+
+    app.put("/updateList", RestController.updateList);
+
+    app.put("/updateOrder", RestController.updateOrder);
+
+    app.put("/addToCart", RestController.addToCart);
+
+    app.delete("/deleteList", RestController.deleteList);
+}
